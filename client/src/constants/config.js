9adc75a5b7e56cfe7126293e27bff87c2bb7b36c
Pulak Mahata <113708302+pulak-mahata@users.noreply.github.com>
2023-09-01 15:35:30 +0530

    Update config.js
    / add

--- client/src/constants/config.js
+++ client/src/constants/config.js
@@ -56,11 +56,11 @@ export const SERVICE_URLS = {
     getAllPosts: { url: '/posts', method: 'GET', params: true },
     getRefreshToken: { url: '/token', method: 'POST' },
     uploadFile: { url: 'file/upload', method: 'POST' },
-    createPost: { url: 'create', method: 'POST' },
-    deletePost: { url: 'delete', method: 'DELETE', query: true },
-    getPostById: { url: 'post', method: 'GET', query: true },
+    createPost: { url: '/create', method: 'POST' },
+    deletePost: { url: '/delete', method: 'DELETE', query: true },
+    getPostById: { url: '/post', method: 'GET', query: true },
     newComment: { url: '/comment/new', method: 'POST' },
-    getAllComments: { url: 'comments', method: 'GET', query: true },
-    deleteComment: { url: 'comment/delete', method: 'DELETE', query: true },
+    getAllComments: { url: '/comments', method: 'GET', query: true },
+    deleteComment: { url: '/comment/delete', method: 'DELETE', query: true },
     updatePost: { url: '/update', method: 'PUT', query: true }
 }
